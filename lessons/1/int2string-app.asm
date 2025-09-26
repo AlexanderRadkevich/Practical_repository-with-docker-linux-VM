70dc1d930dacfda538c8159cd1131a08c208a354
AlexanderRadkevich <alexander1radkevich2git3hud4@gmail.com>
2025-09-26 11:47:36 +0300

    Update int2string-app.asm

--- lessons/1/int2string-app.asm
+++ lessons/1/int2string-app.asm
@@ -1,45 +1,59 @@
-
 section .text
-
 global _start
 
-; 1. entry point
 _start:
-    ; 2. prepare parameters to function call
-    mov     eax, 123456789
-    mov     esi, buffer
-    ; 3. call function `int2str`
-    call    int2str
+    mov eax, 1234512345
+    mov esi, buffer
+    call int2str
 
-    ; 4. prepare parameters to function call
-    mov     eax, 4      ; function number: 4-print
-    mov     ebx, 1      ; device number:   1-stdout
-    mov     ecx, esi    ; buffer to print
-    mov     edx, 1      ; length
-    ; 5. OS call, print to console
-    int     0x80
 
-    ; 6. prepare parameters to function call
-    mov     eax, 1
-    ; 7. OS call, exit
-    int     0x80
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, buffer
+    mov edx, edi
+    int 0x80
 
-; EAX - input parameter
-; ESI - buffer for output string
+
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
+
+
+
+
 int2str:
-    ;
-    ; remainder = eax % 10
-    ; symbol = remainder + 48
-    ; TODO: loop
-    ; TODO: inc/dec ESI
-    xor edx, edx
+    mov edi, 0
     mov ebx, 10
+    mov ecx, esi
+
+.loop:
+    xor edx, edx
     div ebx
+    add dl, '0'
+    mov [esi], dl
+    inc esi
+    inc edi
+    test eax, eax
+    jnz .loop
 
-    add dl, 48
+    ; Реверс
+    mov eax, edi
+    dec esi
+    mov ebx, ecx
 
-    mov byte [esi], dl
+.reverse:
+    cmp ebx, esi
+    jge .done
+    mov al, [ebx]
+    mov dl, [esi]
+    mov [ebx], dl
+    mov [esi], al
+    inc ebx
+    dec esi
+    jmp .reverse
+
+.done:
     ret
 
 section .bss
-    buffer  resb 11
+buffer resb 11
